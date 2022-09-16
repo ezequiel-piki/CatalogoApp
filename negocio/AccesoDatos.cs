fb2b2477f16f14e6384c9394ed26c8044dc81b2a
ezequiel.piki <ezequielpiki23@gmail.com>
2022-09-16 12:48:46 -0300

    primer commit del proyecto

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+   public class AccesoDatos
+    {
+        private SqlConnection conexionSQL;
+        private SqlCommand comandoSQL;
+        private SqlDataReader lectorSQL;
+
+        public SqlDataReader LectorSQL
+        {
+            get { return lectorSQL; }
+        }
+
+        public AccesoDatos()
+        {
+            conexionSQL = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            comandoSQL = new SqlCommand();
+        }
+
+        public void setQuery(string query)
+        {
+            comandoSQL.CommandType = System.Data.CommandType.Text;
+            comandoSQL.CommandText = query;
+        }
+
+        public void ejecutarLectura()
+        {
+            comandoSQL.Connection = conexionSQL;
+            try
+            {
+
+            conexionSQL.Open();
+            lectorSQL = comandoSQL.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comandoSQL.Connection = conexionSQL;
+            try
+            {
+
+                conexionSQL.Open();
+                comandoSQL.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comandoSQL.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void cerrarConexion()
+        {
+            if (lectorSQL != null)
+                lectorSQL.Close();
+            conexionSQL.Close();
+        }
+    }
+}
